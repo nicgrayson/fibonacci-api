5c051764850c08d37715dd8b95f69199f9a5ca14
Nic Grayson <nic.grayson@banno.com>
2015-08-28 00:30:42 -0500

    filter out negative numbers

--- fibonacci.rb
+++ fibonacci.rb
@@ -7,16 +7,20 @@ get '/' do
   "To use this fibonacci number generator please hit /seed_number"
 end
 get '/:num' do
-  get_n_fibonacci_numbers(params['num'].to_i).to_s
+  if params['num'].to_i >= 0
+    get_n_fibonacci_numbers(params['num'].to_i).to_s
+  else
+    status 400
+  end
 end
 
 private
 def get_n_fibonacci_numbers(num)
   case num
   when 0
-    return [0]
+    return []
   when 1
-    return [0,1]
+    return [0]
   else
     (1..num-2).inject([0,1]) do |result, n|
       result << (result[n - 1] + result[n])
