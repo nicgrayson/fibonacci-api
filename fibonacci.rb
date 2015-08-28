5b1a8c7f0fc0855f503edf3026542537e2876c46
Nic Grayson <nic.grayson@banno.com>
2015-08-28 00:04:52 -0500

    added helper method to calculate the correct array

--- fibonacci.rb
+++ fibonacci.rb
@@ -7,5 +7,19 @@ get '/' do
   "To use this fibonacci number generator please hit /seed_number"
 end
 get '/:num' do
-  "Number provided is: #{params['num']}"
+  get_n_fibonacci_numbers(params['num'].to_i).to_s
+end
+
+private
+def get_n_fibonacci_numbers(num)
+  case num
+  when 0
+    return [0]
+  when 1
+    return [0,1]
+  else
+    (1..num-2).inject([0,1]) do |result, n|
+      result << (result[n - 1] + result[n])
+    end
+  end
 end
